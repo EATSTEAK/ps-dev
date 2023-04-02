2cb417d3a4a4ba57ae82581ba7a7f25e37a3d847
Hyomin Koo <me@eatsteak.dev>
2023-04-02 05:42:20 +0000

    solved 11724

--- 11724/main.cpp
+++ 11724/main.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-bool graph[1001][1001] = {0};
-bool searched[1001] = {0};
+int p[1001];
+long pSum[1001];
+int n;
 
 int main() {
-  int n, m;
-  int r = 0;
-  cin >> n >> m;
-  while(m--) {
-    int u, v;
-    cin >> u >> v;
-    graph[u][v] = true;
-    graph[v][u] = true;
+  cin >> n;
+  for(int i=0;i<n;i++) {
+    int t;
+    cin >> t;
+    p[i] = t;
   }
-  for(int i=1;i<=n;i++) {
-    if(!searched[i]) {
-      queue<int> connected;
-      connected.push(i);
-      searched[i] = true;
-      while(!connected.empty()) {
-        int t = connected.front();
-        connected.pop();
-        for(int i=1;i<=n;i++) {
-          if(graph[t][i] && !searched[i]) {
-            searched[i] = true;
-            connected.push(i);
-          }
-        }
-      }
-      r++;
-    }
+  sort(p, p + n);
+  pSum[0] = p[0];
+  long r = p[0];
+  for(int i=1;i<n;i++) {
+    pSum[i] = pSum[i-1] + p[i];
+    r += pSum[i];
   }
   cout << r << '\n';
 }
