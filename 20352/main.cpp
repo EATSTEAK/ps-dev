24e588dc72e1a0659e20ce34a834ea4f1e22b6e2
Hyomin Koo <me@eatsteak.dev>
2024-07-06 09:40:06 +0000

    solve: 20352

---
+++ 20352/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <cmath>
+
+
+using namespace std;
+
+long long n;
+
+int main() {
+  cin.tie(NULL);
+  ios::sync_with_stdio(false);
+  cin >> n;
+  double r = sqrt(n / M_PI);
+  printf("%.10f", 2 * r * M_PI);
+}
